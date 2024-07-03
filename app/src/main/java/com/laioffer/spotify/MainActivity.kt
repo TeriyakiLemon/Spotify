a1e0715c290d37090d995c988942b4ccc4f14cd1
Tianchen zhang <jasontc196@gmail.com>
2024-07-02 22:57:08 -0400

    update network layer (7/2)

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.laioffer.spotify.network.NetworkApi
+import com.laioffer.spotify.network.NetworkModule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "We are at onCreate()")
         setContentView(R.layout.activity_main)
 
+        // findElementByClass/Id
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-        val navHostFragment =supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
@@ -27,8 +38,15 @@ class MainActivity : AppCompatActivity() {
             navController.popBackStack(it.itemId, inclusive = false)
             true
         }
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val retrofit = NetworkModule.provideRetrofit()
+            val api = retrofit.create(NetworkApi::class.java)
+
+            val response = api.getHomeFeed().execute().body()
+            Log.d("Network", response.toString())
+        }
     }
 }
-
-
 
